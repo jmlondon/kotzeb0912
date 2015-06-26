907aba24b42b7c23d992a112d1448203d58f08aa
Josh London <josh.london@noaa.gov>
2015-06-26 14:43:00 -0700

    ADD data-open DIRECTORY AND ADD JSON FILES

--- data-raw/001-get-data.R
+++ data-raw/001-get-data.R
@@ -1,5 +1,6 @@
 library(wcUtils)
 library(dplyr)
+library(jsonlite)
 
 # first thing, get deployment data from WCDP
 r <- wcPOST()
@@ -26,7 +27,7 @@ date_unique <-
   do(unique_date = xts::make.time.unique(.$date_time,eps=1)) %>%
   tidyr::unnest(unique_date) %>%
   mutate(unique_posix = as.POSIXct(.$unique_date,origin='1970-01-01 00:00:00',tz='UTC')) %>%
-  dplyr::arrange(deployid,unique_posix)
+  dplyr::arrange(deployid,unique_posix) %>% dplyr::select(unique_posix)
 
 kotz_data$locations <- arrange(kotz_data$locations,deployid,date_time) %>%
   bind_cols(date_unique)
@@ -35,3 +36,22 @@ kotz_data$status <-
   dplyr::rbind_all(plyr::llply(data_list,function (x) x$status))
 
 # we need to now address all the histo data
+
+
+# create our data products and save the RData files
+kotzeb0912_locs <- kotz_data$locations
+kotzeb0912_status <- kotz_data$status
+save(kotzeb0912_locs,file='data/kotzeb0912_locs.RData')
+save(kotzeb0912_status,file='data/kotzeb0912_status.RData')
+
+# create open JSON formats and save to data-open
+json.out <- toJSON(kotzeb0912_locs,pretty=TRUE)
+file.output <- file("data-open/kotzeb0912_locs.json")
+writeLines(json.out, file.output)
+close(file.output)
+
+json.out <- toJSON(kotzeb0912_status,pretty=TRUE)
+file.output <- file("data-open/kotzeb0912_status.json")
+writeLines(json.out, file.output)
+close(file.output)
+
