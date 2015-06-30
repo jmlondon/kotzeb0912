d0f5ce00c8642bbf660919f40d28d1768d33f00f
Josh London <josh.london@noaa.gov>
2015-06-30 15:32:24 -0700

    ADD SUPPORT FOR DIVE DEPTH, DURATION, TAD DATA

--- data-raw/001-get-data.R
+++ data-raw/001-get-data.R
@@ -36,13 +36,66 @@ kotz_data$status <-
   dplyr::rbind_all(plyr::llply(data_list,function (x) x$status))
 
 # we need to now address all the histo data
+# first, we'll find all the histos with identified limits
+limits_idx <- unlist(lapply(data_list,function(x) !is.null(x$histos$limits)))
+sub_dat <- data_list[limits_idx]
+sub_dat <- lapply(sub_dat,function(x) x$histos)
 
+# now create a function to extract the limit values
+get_limits <- function(histo_list,limittype) {
+  dd_lmts <- dplyr::filter(histo_list$limits,histtype==limittype) %>%
+    tidyr::gather(key=bin,value=limits,bin1:bin14) %>%
+    dplyr::select(-histtype)
+}
+
+# first up, dive depths
+dd_dat <- lapply(sub_dat,function(x) wcUtils::tidyDiveDepths(x))
+dd_limits <- lapply(sub_dat,function(x) get_limits(x,'DiveDepthLIMITS'))
+
+dd_list <- vector("list",length=length(dd_dat))
+
+for(i in 1:length(dd_dat)) {
+  dd_list[[i]] <- dplyr::left_join(dd_dat[[i]],dd_limits[[i]])
+}
+
+dd_dat <- dplyr::bind_rows(dd_list)
+kotzeb0912_depths <- dd_dat
+
+# next, dive duration
+dd_dat <- lapply(sub_dat,function(x) wcUtils::tidyDiveDurations(x))
+dd_limits <- lapply(sub_dat,function(x) get_limits(x,'DiveDurationLIMITS'))
+
+dd_list <- vector("list",length=length(dd_dat))
+
+for(i in 1:length(dd_dat)) {
+  dd_list[[i]] <- dplyr::left_join(dd_dat[[i]],dd_limits[[i]])
+}
+
+dd_dat <- dplyr::bind_rows(dd_list)
+kotzeb0912_durations <- dd_dat
+
+# next, time-at-depth
+tad_dat <- lapply(sub_dat,function(x) wcUtils::tidyTimeAtDepth(x))
+tad_limits <- lapply(sub_dat,function(x) get_limits(x,'TADLIMITS'))
+
+tad_list <- vector("list",length=length(tad_dat))
+
+for(i in 1:length(tad_dat)) {
+  tad_list[[i]] <- dplyr::left_join(tad_dat[[i]],tad_limits[[i]])
+}
+
+tad_dat <- dplyr::bind_rows(tad_list)
+kotzeb0912_tad <- tad_dat
 
 # create our data products and save the RData files
 kotzeb0912_locs <- kotz_data$locations
 kotzeb0912_status <- kotz_data$status
 save(kotzeb0912_locs,file='data/kotzeb0912_locs.RData')
 save(kotzeb0912_status,file='data/kotzeb0912_status.RData')
+
+save(kotzeb0912_depths,file='data/kotzeb0912_depths.RData')
+save(kotzeb0912_durations,file='data/kotzeb0912_durations.RData')
+save(kotzeb0912_tad,file='data/kotzeb0912_tad.RData')
 
 # create open JSON formats and save to data-open
 json.out <- toJSON(kotzeb0912_locs,pretty=TRUE)
@@ -55,3 +108,17 @@ file.output <- file("data-open/kotzeb0912_status.json")
 writeLines(json.out, file.output)
 close(file.output)
 
+json.out <- toJSON(kotzeb0912_depths,pretty=TRUE)
+file.output <- file("data-open/kotzeb0912_depths.json")
+writeLines(json.out, file.output)
+close(file.output)
+
+json.out <- toJSON(kotzeb0912_durations,pretty=TRUE)
+file.output <- file("data-open/kotzeb0912_durations.json")
+writeLines(json.out, file.output)
+close(file.output)
+
+json.out <- toJSON(kotzeb0912_tad,pretty=TRUE)
+file.output <- file("data-open/kotzeb0912_tad.json")
+writeLines(json.out, file.output)
+close(file.output)
