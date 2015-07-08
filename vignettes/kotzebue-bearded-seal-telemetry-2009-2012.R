47bf4b9c1787483696c108414e848aa1d7eafcbe
Josh London <josh.london@noaa.gov>
2015-07-07 17:23:58 -0700

    SIGNIFICANT UPDATES & EXPANSION OF THE VIGNETTE

--- vignettes/kotzebue-bearded-seal-telemetry-2009-2012.R
+++ vignettes/kotzebue-bearded-seal-telemetry-2009-2012.R
@@ -6,7 +6,7 @@ library(kotzeb0912)
 library(dplyr)
 library(ggmap)
 
-## ----echo=FALSE,fig.width=5,out.extra='style="float: right;"',fig.cap='Tracklines derived from all Argos and GPS location estimates',warning=FALSE,message=FALSE----
+## ----echo=FALSE,fig.width=3.5,out.extra='style="float: right;"',fig.cap='Tracklines derived from all Argos and GPS location estimates',warning=FALSE,message=FALSE----
 data("kotzeb0912_locs")
 
 locs <- kotzeb0912_locs %>% 
@@ -23,4 +23,40 @@ map <- get_map(location=c(med_lng,med_lat),
 gg <- ggmap(map) + geom_point(data=locs,mapping=aes(x=longitude,y=latitude),
                               alpha=0.25,size=0.75)
 gg
+
+## ----data-locs-----------------------------------------------------------
+data("kotzeb0912_locs")
+dplyr::glimpse(kotzeb0912_locs)
+
+## ----unique-instr--------------------------------------------------------
+unique(kotzeb0912_locs$instr)
+
+## ----range-datetime------------------------------------------------------
+range(kotzeb0912_locs$date_time)
+
+## ----unique-type---------------------------------------------------------
+unique(kotzeb0912_locs$type)
+
+## ----unique_quality------------------------------------------------------
+unique(kotzeb0912_locs$quality)
+
+## ----data-status---------------------------------------------------------
+data("kotzeb0912_status")
+dplyr::glimpse(kotzeb0912_status)
+
+## ----data-depths---------------------------------------------------------
+data("kotzeb0912_depths")
+dplyr::glimpse(kotzeb0912_depths)
+
+## ----data-durations------------------------------------------------------
+data("kotzeb0912_durations")
+dplyr::glimpse(kotzeb0912_durations)
+
+## ----data-tad------------------------------------------------------------
+data("kotzeb0912_tad")
+dplyr::glimpse(kotzeb0912_tad)
+
+## ----data-timelines------------------------------------------------------
+data("kotzeb0912_timelines")
+dplyr::glimpse(kotzeb0912_timelines)
 
